db5799f50e067df685ae2bdae5855fc3f01bea93
Imran <polboy777@gmail.com>
2020-04-03 23:38:07 +0600

    remove home top links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,6 @@
 
     <div class="destinations">
       <div v-for="destination in destinations" :key="destination.name">
-        <router-link :to="destination.slug">
-          <h2>{{ destination.name }}</h2>
-        </router-link>
-
         <figure>
           <router-link :to="{ name: 'DestinationDetails', params: { slug: destination.slug } }">
             <img :src="require(`@/assets/${destination.image}`)" :alt="destination.name" />
