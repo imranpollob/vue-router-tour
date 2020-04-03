5e915da57ee7d9f2837c99b848c0c48152873ed7
Imran <polboy777@gmail.com>
2020-04-03 23:35:39 +0600

    experiences

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,41 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '@/views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
     path: "/destination/:slug",
     name: "DestinationDetails",
-    component: () => import(/* webpackChunkName: "DestinationDetails"*/ "@/views/DestinationDetails"),
-    props: true
+    component: () =>
+      import(
+        /* webpackChunkName: "DestinationDetails"*/ "@/views/DestinationDetails"
+      ),
+    props: true,
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experienceDetails",
+        props: true,
+        component: () =>
+          import(
+            /*webpackChunkName: "ExperienceDetails"*/ "@/views/ExperienceDetails"
+          )
+      }
+    ]
   }
-]
+];
 
 const router = new VueRouter({
   mode: "history",
   linkExactActiveClass: "rename-active-class",
   routes
-})
+});
 
-export default router
+export default router;
